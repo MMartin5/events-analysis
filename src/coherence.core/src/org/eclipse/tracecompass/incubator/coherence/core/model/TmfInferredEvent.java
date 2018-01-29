08029f96549e3d775307f7fc30629286235cdb90
Marie Martin <marie.martin@etu.utc.fr>
2018-01-29 10:44:14 -0500

    Infer values when the event field is in a 'stateValue' tag.
    Handle multiple event fields in the same condition.
    
    Add an exception when there are several fields with the same name (which
    means that the same field has been inferred several times, with different
    results).
    Note: this exception needs to be handled (what should we do when that
    happens?).

--- src/coherence.core/src/org/eclipse/tracecompass/incubator/coherence/core/model/TmfInferredEvent.java
+++ src/coherence.core/src/org/eclipse/tracecompass/incubator/coherence/core/model/TmfInferredEvent.java
@@ -113,6 +113,7 @@ public class TmfInferredEvent extends TmfEvent {
         /* Get the conditions in the inferred transition */
         String conditionStr = inferredTransition.to().getCondition();
         String[] conditions = conditionStr.split(":");
+        Map<String, Object> fieldPairs = new HashMap<>();
         for (String cond : conditions) {
         	TmfXmlTransitionValidator validator = testMap.get(cond);
         	ITmfXmlCondition xmlCond = validator.getCondition();
@@ -121,15 +122,21 @@ public class TmfInferredEvent extends TmfEvent {
         	 * so we don't need to consider this case
         	 */
         	if (xmlCond instanceof TmfXmlCondition) {
-        		List<Pair<String, Object>> fieldPairs = inferFromCondition(xmlCond, stateSystem, event, scenarioInfo);
-        		// Construct fields with each pair and add them to the "content field" structure
-    			for (Pair<String, Object> fieldPair : fieldPairs) {
-	    			TmfEventField field = new TmfEventField(fieldPair.getFirst(), fieldPair.getSecond(), null);
-	    			fields.add(field);
-    			}
+        		Map<String, Object> fieldsForCond = inferFromCondition(xmlCond, stateSystem, event, scenarioInfo);
+        		for (String fieldName : fieldsForCond.keySet()) {
+	        		if (fieldsForCond.containsKey(fieldName)) { // it could happen because the same event field could be used in several conditions
+	    				// TODO handle this case
+	    			}
+	        		fieldPairs.put(fieldName, fieldsForCond.get(fieldName));
+        		}
         	}
         }
-        
+    	// Construct fields with each pair and add them to the "content field" structure
+		for (String fieldName : fieldPairs.keySet()) {
+			TmfEventField field = new TmfEventField(fieldName, fieldPairs.get(fieldName), null);
+			fields.add(field);
+		}
+		
 		return new TmfEventField(ITmfEventField.ROOT_FIELD_ID, null, fields.toArray(new TmfEventField[fields.size()]));
 	}
 	
@@ -147,15 +154,16 @@ public class TmfInferredEvent extends TmfEvent {
 	 * @return
 	 * 				A list of pairs (field name, field value)
 	 */
-	private static List<Pair<String, Object>> inferFromCondition(ITmfXmlCondition xmlCond, ITmfStateSystem stateSystem, 
+	private static Map<String, Object> inferFromCondition(ITmfXmlCondition xmlCond, ITmfStateSystem stateSystem, 
 			ITmfEvent event, TmfXmlScenarioInfo scenarioInfo) {
 		
-		List<Pair<String, Object>> fields = new ArrayList<>();
+		Map<String, Object> fields = new HashMap<>();
 		List<ITmfXmlStateValue> stateValues = ((TmfXmlCondition) xmlCond).getStateValues();
 		/* Get value according to what is needed for the condition to be true */
 		for (ITmfXmlStateValue value : stateValues) { // add one event field for each attribute
+			Map<String, Object> tentativeFields = new HashMap<>();
 			/* Extract field name, then associate it with a field value */
-			TmfXmlStateValue stateValue = (TmfXmlStateValue) value;							// Case 1: condition is a field								
+			TmfXmlStateValue stateValue = (TmfXmlStateValue) value;							// Case 1: condition is a field
 			String fieldName = stateValue.getEventField(); // not null in case of XML tag <field ..>
 			Object fieldValue = null;
 			if (fieldName == null) {														// Case 2: condition is a state value with type eventField
@@ -166,16 +174,15 @@ public class TmfInferredEvent extends TmfEvent {
     			}        				
     			if (fieldName == null) {													// Case 3: condition is a state attribute sequence with a type eventField
     				List<ITmfXmlStateAttribute> attributes = stateValue.getAttributes();
-    				Pair<List<String>, Pair<String, Integer>> res = getPathFromAttributes(attributes);
+    				Pair<List<String>, List<Pair<String, Integer>>> res = getPathFromAttributes(attributes);
     				List<String> path = res.getFirst();
-    				fieldName = res.getSecond().getFirst();
-    				int fieldIndex = res.getSecond().getSecond();
-    				String[] pattern = path.toArray(new String[path.size()]);
+    				List<Pair<String, Integer>> resFields = res.getSecond();
     		
-    				if (fieldName == null) {												// Case 4 (default): condition is not about an event field
+    				if (resFields.isEmpty()) {												// Case 4 (default): condition is not about an event field
     					continue;
     				}
     				else { // get value for case 3 : depends on the state value following this sequence of stateAttribute tags
+    					String[] pattern = path.toArray(new String[path.size()]);
     					/* Get every possible path (where missing event field is a wildcard) */
     					List<Integer> quarks = stateSystem.getQuarks(pattern);
     					/* Get the following state value for comparison */
@@ -190,40 +197,70 @@ public class TmfInferredEvent extends TmfEvent {
     					for (Integer quark : quarks) {
     						ITmfStateValue currentValue = stateSystem.queryOngoingState(quark);
     						if (currentValue == compValue) { // we found a match for the desired value
-    							/* Find the missing field value in the matching path */
-    							fieldValue = stateSystem.getFullAttributePathArray(quark)[fieldIndex]; // FIXME several quarks could match => which one should we choose? (for now, we choose the first one) 
+    							for (Pair<String, Integer> resField : resFields) {
+    								fieldName = resField.getFirst();
+    								/* Find the missing field value in the matching path */
+    								int fieldIndex = resField.getSecond();
+        							fieldValue = stateSystem.getFullAttributePathArray(quark)[fieldIndex]; // FIXME several quarks could match => which one should we choose? (for now, we choose the first one)
+        							tentativeFields.put(fieldName, fieldValue);
+    							} 
     							break;
     						}
     					}
     				}
     			}
     			else { // get value for case 2
-    				// field value is the other state value that we are trying to compare it to
-    				// or is the value of the preceding sequence of state attributes
-    				// or is the value of the preceding field tag
-    				/* Look for the quark, starting from root and descending until the last attribute is reached */
-					int quark = IXmlStateSystemContainer.ROOT_QUARK;
-					for (ITmfXmlStateAttribute attr : value.getAttributes()) {
-						quark = attr.getAttributeQuark(event, quark, scenarioInfo);
-					}
-					/* Find value by querying the state system */
-					try {
-						fieldValue = stateSystem.querySingleState(event.getTimestamp().getValue(), quark).getValue();
-					} catch (StateSystemDisposedException e) {
-						Activator.logError("State system disposed while trying to get the value of inferred event", e); //$NON-NLS-1$
-		                continue;
-					}
+    				// field value is the other state value that we are trying to compare it to (2.1)
+    				// or is the value of the preceding sequence of state attributes (2.2)
+    				// or is the value of the preceding field tag (2.3)
+    				
+    				if (stateValues.size() == 2) {											// case 2.1
+						try {
+							/* (1 - index) because there is only 2 state values 
+							 * so it's either the preceding if event field is the second 
+							 * or the following if event field is the first */
+							fieldValue = stateValues.get(1 - stateValues.indexOf(value)).getValue(event, scenarioInfo);
+							tentativeFields.put(fieldName, fieldValue);
+						} catch (AttributeNotFoundException e) {
+							Activator.logError("Attribute not found while trying to get the value of inferred event", e); //$NON-NLS-1$
+			                continue;
+						}
+    				}
+    				else {																	// case 2.2
+	    				/* Look for the quark, starting from root and descending until the last attribute is reached */
+						int quark = IXmlStateSystemContainer.ROOT_QUARK;
+						for (ITmfXmlStateAttribute attr : value.getAttributes()) {
+							quark = attr.getAttributeQuark(event, quark, scenarioInfo);
+						}
+						/* Find value by querying the state system */
+						try {
+							fieldValue = stateSystem.querySingleState(event.getTimestamp().getValue(), quark).getValue();
+							tentativeFields.put(fieldName, fieldValue);
+						} catch (StateSystemDisposedException e) {
+							Activator.logError("State system disposed while trying to get the value of inferred event", e); //$NON-NLS-1$
+			                continue;
+						}
+    				}
+					
+    				// case 2.3
+    				// TODO can we retrieve it? because it depends on a field tag value, which is lost too (confirm?)
     			}
 			}
 			else { // get value for case 1 : the state value following this field tag
 				try {
 					fieldValue = stateValues.get(stateValues.indexOf(value) + 1).getValue(event, scenarioInfo);
+					tentativeFields.put(fieldName, fieldValue);
 				} catch (AttributeNotFoundException e) {
 					Activator.logError("Attribute not found while trying to get the value of inferred event", e); //$NON-NLS-1$
 	                continue;
 				}
 			}
-			fields.add(new Pair<String, Object>(fieldName, fieldValue));
+			for (String tentativeName : tentativeFields.keySet()) {
+				if (fields.containsKey(tentativeName)) { // it could happen because the same event field could be used several times in this condition
+					// TODO handle this case
+				}
+				fields.put(tentativeName, tentativeFields.get(tentativeName));
+			}
 		}
 		return fields;
 	}
@@ -231,25 +268,24 @@ public class TmfInferredEvent extends TmfEvent {
 	/**
 	 *  Create a state system path from attribute's names, with unknown event field values replaced by wildcard '*' 
 	 */
-	private static Pair<List<String>, Pair<String, Integer>> getPathFromAttributes(List<ITmfXmlStateAttribute> attributes) {
+	private static Pair<List<String>, List<Pair<String, Integer>>> getPathFromAttributes(List<ITmfXmlStateAttribute> attributes) {
 		List<String> path = new ArrayList<>();
-		Pair<String, Integer> field = null;
+		List<Pair<String, Integer>> fields = new ArrayList<>();
 		for (ITmfXmlStateAttribute attribute : attributes) {
 			TmfXmlStateAttribute stateAttribute = (TmfXmlStateAttribute) attribute;
-			// FIXME we need to deal with type location, where the event field could be inside of the location definition
 			if (stateAttribute.getType() == TmfXmlStateAttribute.StateAttributeType.EVENTFIELD) { // handle event field
 				// FIXME could be several event field (possibly one per attribute)
-				field = new Pair<String, Integer>(stateAttribute.getName(), attributes.indexOf(stateAttribute)); 
+				fields.add(new Pair<String, Integer>(stateAttribute.getName(), attributes.indexOf(stateAttribute))); 
 				path.add("*");
 			}
 			else if (stateAttribute.getType() == TmfXmlStateAttribute.StateAttributeType.LOCATION) {
 				for (TmfXmlLocation location : stateAttribute.getContainer().getLocations()) {
 					if (location.getId().equals(stateAttribute.getName())) { // look for the location object
 						List<ITmfXmlStateAttribute> locationAttributes = location.getPath();
-						Pair<List<String>, Pair<String, Integer>> locationMap = getPathFromAttributes(locationAttributes);
+						Pair<List<String>, List<Pair<String, Integer>>> locationMap = getPathFromAttributes(locationAttributes);
 						path.addAll(locationMap.getFirst());
-						if (locationMap.getSecond() != null) {
-							field = locationMap.getSecond(); 
+						if (!locationMap.getSecond().isEmpty()) {
+							fields.addAll(locationMap.getSecond()); 
 						}
 					}
 				}
@@ -258,7 +294,7 @@ public class TmfInferredEvent extends TmfEvent {
 				path.add(stateAttribute.getName());
 			}
 		}
-		return new Pair<List<String>, Pair<String, Integer>>(path, field);
+		return new Pair<List<String>, List<Pair<String, Integer>>>(path, fields);
 	}
 	
 	private TmfInferredEvent(final ITmfTrace trace,
